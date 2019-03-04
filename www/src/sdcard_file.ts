e12ae114e3e4db561d3089fe5dcc9ba43e2b630a
Alkihis <alkihis@gmail.com>
2019-03-04 17:03:28 +0100

    added comments

--- www/src/sdcard_file.ts
+++ www/src/sdcard_file.ts
@@ -1,6 +1,11 @@
 import { SDCARD_PATH } from "./main";
 import { writeFileFromEntry } from "./helpers";
 
+/**
+ * Fonction de test
+ * @param path Répertoire à lister
+ * @param prefix Base / racine
+ */
 export async function listSdCard(path: string = "", prefix: string = SDCARD_PATH) : Promise<void> {
     const dir = await getSdCardDir(path, prefix) as DirectoryEntry;
 
@@ -15,24 +20,45 @@ export async function listSdCard(path: string = "", prefix: string = SDCARD_PATH
     );
 }
 
+/**
+ * resolveLocalFileSystemURL version Promise
+ * @param url URL à résoudre
+ */
 export function resolveFSURL(url: string) : Promise<Entry> {
     return new Promise((resolve, reject) => {
         window.resolveLocalFileSystemURL(url, resolve, reject);
     });
 }
 
+/**
+ * Obtient un répertoire depuis une entrée. Le crée si besoin.
+ * @param entry Entrée de répertoire parent
+ * @param name Nom du répertoire à obtenir
+ * @param create Créer le répertoire
+ */
 export function getDirectoryFromEntry(entry: DirectoryEntry, name: string, create = true) : Promise<DirectoryEntry> {
     return new Promise((resolve, reject) => {
         entry.getDirectory(name, { create, exclusive: false }, resolve, reject);
     });
 }
 
+/**
+ * Obtient un fichier depuis une entrée de répertoire. Le crée si besoin
+ * @param entry Entrée du répertoire parent
+ * @param name Nom du fichier
+ * @param create Créer le fichier
+ */
 export function getFileFromEntry(entry: DirectoryEntry, name: string, create = true) : Promise<FileEntry> {
     return new Promise((resolve, reject) => {
         entry.getFile(name, { create, exclusive: false }, resolve, reject);
     });
 }
 
+/**
+ * Obtenir un répertoire. Retourne null si le répertoire ne peut pas être atteint (pas une promesse rompue !)
+ * @param name Nom du répertoire à récupérer
+ * @param root Racine de la carte SD (automatiquement définie par défaut)
+ */
 export async function getSdCardDir(name: string = "", root: string = SDCARD_PATH) : Promise<DirectoryEntry> {
     let folder: DirectoryEntry;
 
@@ -54,6 +80,10 @@ export async function getSdCardDir(name: string = "", root: string = SDCARD_PATH
     }
 }
 
+/**
+ * Obtient un fichier présent dans la carte SD
+ * @param name Nom complet du fichier (répertoire compris)
+ */
 export async function getSdCardFile(name: string) : Promise<FileEntry> {
     const path = name.split('/');
     name = path.pop();
@@ -68,15 +98,23 @@ export async function getSdCardFile(name: string) : Promise<FileEntry> {
     return getFileFromEntry(folder, name);
 }
 
+/**
+ * Ecrit un fichier sur la carte SD. Le crée si besoin.
+ * @param path Chemin du fichier
+ * @param content Contenu du fichier
+ */
 export async function writeSdCardFile(path: string, content: Blob) : Promise<void> {
     const file = await getSdCardFile(path);
 
-    console.log(file);
     if (file) {
         return writeFileFromEntry(file, content);
     }
 }
 
+/**
+ * Supprime un fichier de la carte SD
+ * @param path Chemin du fichier
+ */
 export async function removeSdCardFile(path: string) : Promise<void> {
     const file = await getSdCardFile(path);
 
@@ -85,6 +123,10 @@ export async function removeSdCardFile(path: string) : Promise<void> {
     });
 }
 
+/**
+ * Obtient les répertoires sur cartes SD montés. 
+ * Attention, depuis KitKat, la racine de la carte SD n'est PAS accessible en écriture !
+ */
 export function getSdCardFolder() : Promise<{path: string, filePath: string, canWrite: boolean}[]> {
     return new Promise((resolve, reject) => {
         // @ts-ignore
